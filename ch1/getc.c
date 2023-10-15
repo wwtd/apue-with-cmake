0bdd103c7875f9e30518521784cf0c1511fd46db
felix <chentengfei1998@gmail.com>
2023-10-15 21:54:24 +0800

    add some src

---
+++ ch1/getc.c
@@ -0,0 +1,20 @@
+#include "apue.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+    while ((c = getc(stdin)) != EOF)
+    {
+        if (putc(c, stdout) == EOF)
+        {
+            err_sys("output error");
+        }
+    }
+    if (ferror(stdin))
+    {
+        err_sys("input error");
+    }
+
+    exit(0);
+}
